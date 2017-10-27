67a8d3a2225d4af41cb5be91613b8088fed0cd98
Nick Nizovtsev <nizovtsevnv@gmail.com>
2017-10-27 11:07:47 +0400

    Hide menu bar and fix filters bar

--- app/assets/javascripts/arctic_admin/base.js
+++ app/assets/javascripts/arctic_admin/base.js
@@ -3,52 +3,64 @@
 //= require active_admin/base
 
 $(function() {
+  var animationFilterDone = true;
   $('#sidebar').click(function (e) {
     var position = $(this).position();
     var width = $(this).width();
-    if (e.pageX < position.left) {
-      if ($(this).css('right') == '0px') {
-        $(this).css('position', 'fixed');
-        $(this).animate({
-          right: "-="+width
-        }, 600, function() {
-          animationFilterDone = true;
-        });
-      } else {
-        $(this).animate({
-          right: "+="+width
-        }, 600, function() {
-          $(this).css('position', 'absolute');
-          animationFilterDone = true;
-        });
+    //if (e.pageX < position.left) {
+      if(animationFilterDone == true) {
+        animationFilterDone = false;
+        if ($(this).css('right') == '0px') {
+          $(this).css('position', 'fixed');
+          $(this).animate({
+            right: "-="+width
+          }, 300, function() {
+            animationFilterDone = true;
+          });
+        } else {
+          document.body.scrollTop = document.documentElement.scrollTop = 0;
+          $(this).animate({
+            right: "+="+width
+          }, 300, function() {
+            $(this).css('position', 'absolute');
+            animationFilterDone = true;
+          });
+        }
       }
-    }
+    //}
   });
 
   var animationDone = true;
   $('#utility_nav').click(function (e) {
+    console.log(e.pageX);
     var position = $(this).position();
     var tabs = $('#tabs');
     var width = Math.round(tabs[0].getBoundingClientRect().width);
+    console.log(position.left);
+    console.log(tabs);
+    console.log(width);
 
-    if (e.pageX < (position.left + 40)) {
+    //if (e.pageX < (position.left + 40)) {
       if(animationDone == true) {
         animationDone = false;
         if (tabs.css('left') == '0px') {
+          //tabs.css('position', 'fixed');
           tabs.animate({
             left: "-="+width
-          }, 400, function() {
+          }, 300, function() {
             animationDone = true;
           });
         } else {
+          //document.body.scrollTop = document.documentElement.scrollTop = 0;
           tabs.animate({
             left: "+="+width
-          }, 400, function() {
+          }, 300, function() {
+            //tabs.css('position', 'fixed');
             animationDone = true;
           });
         }
       }
-    }
+    //}
   });
 
   $('body').click(function (e) {
@@ -60,7 +72,7 @@ $(function() {
           animationDone = false;
           tabs.animate({
             left: "-="+width
-          }, 400, function() {
+          }, 300, function() {
             animationDone = true;
           });
         }
